e5bdb52c9ad1b95cb0df5b24f4c3e7d85b405ebf
hoomzfwp <ndamte@gmail.com>
2020-09-09 21:27:27 -0500

    Implement randomized quicksort

--- src/SortingMethods.java
+++ src/SortingMethods.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SortingMethods {
 
     public static void quicksort(TestInteger A[], int p, int r){
@@ -27,4 +29,24 @@ public class SortingMethods {
         return i + 1;
     }
 
+
+    public static void randomizedQuicksort(TestInteger A[], int p, int r){
+        if(p < r){
+            int q = randomizedPartition(A, p, r);
+            randomizedQuicksort(A, p, q-1);
+            randomizedQuicksort(A,q + 1, r);
+        }
+    }
+
+    private static int randomizedPartition(TestInteger A[], int p, int r){
+        Random rand = new Random();
+        int i = rand.nextInt((r - p) + 1) + p;
+        TestInteger swap = new TestInteger();
+        TestInteger swap2 = new TestInteger();
+        swap = A[i];
+        swap2 = A[r];
+        A[r] = swap;
+        A[i] = swap2;
+        return partition(A, p, r);
+    }
 }
